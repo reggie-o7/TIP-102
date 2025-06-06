a51ccc03c68b3f4d8644641d5d9153221da4084f
Reginald Holmes <thebo@spoon.(none)>
2025-06-05 22:25:43 -0400

    Problem 2: Greeting Complete

--- TIP Unit 1 Problems.py
+++ TIP Unit 1 Problems.py
@@ -1,6 +1,11 @@
 # Problem 1: Hundred Acre Wood
 def welcome():
 	print("Welcome to The Hundred Acre Wood!")
+     
+# Problem 2: Greeting
+def greeting(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin")
+	
 
 #################################################
 
@@ -8,6 +13,12 @@ def main():
     # P1
     welcome()
 
+    # P2
+    greeting("Michael")
 
+    # P3
+    
+
+    print("All Test cases passed!")
 if __name__ == "__main__":
     main()
