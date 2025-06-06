0475be33218c0a1364d83cddb6080783c29d5d70
Reginald Holmes <thebo@spoon.(none)>
2025-06-05 22:12:22 -0400

    Problem 1: Hundred Acre Wood Comeplete

---
+++ TIP Unit 1 Problems.py
@@ -0,0 +1,13 @@
+# Problem 1: Hundred Acre Wood
+def welcome():
+	print("Welcome to The Hundred Acre Wood!")
+
+#################################################
+
+def main():
+    # P1
+    welcome()
+
+
+if __name__ == "__main__":
+    main()
