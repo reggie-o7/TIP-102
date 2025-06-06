b602dd28ed405d7a630b356f2ad2541c09171a14
Reginald Holmes <thebo@spoon.(none)>
2025-06-05 22:42:18 -0400

    Problem 3: Catchphrase Complete

--- TIP Unit 1 Problems.py
+++ TIP Unit 1 Problems.py
@@ -5,7 +5,16 @@ def welcome():
 # Problem 2: Greeting
 def greeting(name):
     print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin")
-	
+
+# Problem 3: Catchphrase
+def print_catchphrase(character):
+    characters = {"Pooh":"Oh bother!","Tigger":"TTFN: Ta-ta for now!","Eeyore":"Thanks for noticing me.","Christopher Robin":"Silly old bear."}
+    for key,value in characters.items():
+        if key == character:
+            return value
+    return f"Sorry! I don't know {character}'s catchphrase!"
+        
+
 
 #################################################
 
@@ -17,7 +26,7 @@ def main():
     greeting("Michael")
 
     # P3
-    
+    assert print_catchphrase("Tigger") == "TTFN: Ta-ta for now!"
 
     print("All Test cases passed!")
 if __name__ == "__main__":
