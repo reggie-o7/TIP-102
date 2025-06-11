4544d21fb460cdce78a2dd8f56fce9798a0596cc
reggie-o7 <reginaldfholmes@gmail.com>
2025-06-11 06:40:18 -0400

    Update TIP Unit 2 Problems.py
    Problem 1 complete

--- TIP Unit 2 Problems.py
+++ TIP Unit 2 Problems.py
@@ -2,7 +2,10 @@
 # Advanced Problem Set Version 1
 # Problem 1: Counting Treasure
 def total_treasure(treasure_map):
-    pass
+    total = 0
+    for key, value in treasure_map.items():
+        total += value
+    return total
 
 treasure_map1 = {
     "Cove": 3,
@@ -10,20 +13,14 @@ treasure_map1 = {
     "Forest": 5
 }
 
-treasure_map2 = {
-    "Shipwreck": 10,
-    "Cave": 20,
-    "Lagoon": 15,
-    "Island Peak": 5
-}
 
-print(total_treasures(treasure_map1)) 
-print(total_treasures(treasure_map2)) 
+
 
 def main():
     # Breakout Problems Session 1
     # Standard Problem Set Version 1
     # P1
+    assert (total_treasures(treasure_map1)) == 15
         
     print("All Test cases passed!")
 if __name__ == "__main__":
