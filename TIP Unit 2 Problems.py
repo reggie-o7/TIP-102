143ffed0fc2ca96673454c2e7c3e5758ea0ae994
Reginald Holmes <thebo@spoon.(none)>
2025-06-11 02:28:17 -0400

    Initial commit

---
+++ TIP Unit 2 Problems.py
@@ -0,0 +1,14 @@
+# Breakout Problems Session 1
+# Advanced Problem Set Version 1
+# Problem 1: Counting Treasure
+def total_treasure(treasure_map):
+    pass
+
+def main():
+    # Breakout Problems Session 1
+    # Standard Problem Set Version 1
+    # P1
+        
+    print("All Test cases passed!")
+if __name__ == "__main__":
+    main()
