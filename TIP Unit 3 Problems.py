9234e3c18d9cc91f7189a438a2ed9075dc20400a
Reginald Holmes <thebo@spoon.(none)>
2025-06-15 16:40:07 -0400

    Adv sess 1 Q1 complete

--- TIP Unit 3 Problems.py
+++ TIP Unit 3 Problems.py
@@ -2,8 +2,24 @@
 # Advanced Problem Set Version 1
 # Problem 1: Arrange Guest Arrival Order
 def arrange_guest_arrival_order(arrival_pattern):
+    res = []
     stack = []
 
+    for i in range(len(arrival_pattern) + 1):
+        stack.append(i + 1)
+            
+        if i == len(arrival_pattern) or arrival_pattern[i] == 'I':
+            while stack:
+                res.append(stack.pop())
+    return ''.join(map(str, res))
 
 print(arrange_guest_arrival_order("IIIDIDDD"))  
 print(arrange_guest_arrival_order("DDD"))  
+
+# Problem 2: Reveal Attendee List in Order
+def reveal_attendee_list_in_order(attendees):
+  pass
+
+print(reveal_attendee_list_in_order([17,13,11,2,3,5,7])) 
+print(reveal_attendee_list_in_order([1,1000]))  
+
