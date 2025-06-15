bbee33fead5de1b5c624b26e9a32516863f76a99
Reginald Holmes <thebo@spoon.(none)>
2025-06-15 15:50:37 -0400

    Initial Commit

---
+++ TIP Unit 3 Problems.py
@@ -0,0 +1,9 @@
+# Breakout Problems Session 1
+# Advanced Problem Set Version 1
+# Problem 1: Arrange Guest Arrival Order
+def arrange_guest_arrival_order(arrival_pattern):
+    stack = []
+
+
+print(arrange_guest_arrival_order("IIIDIDDD"))  
+print(arrange_guest_arrival_order("DDD"))  
