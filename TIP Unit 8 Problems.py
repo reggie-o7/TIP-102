0d767ec7dbefa11c103e9ef9260d7b2a1ee09581
Reginald Holmes <thebo@spoon.(none)>
2025-07-27 15:12:28 -0400

    final commit

--- TIP Unit 8 Problems.py
+++ TIP Unit 8 Problems.py
@@ -238,7 +238,7 @@ def harvest_berries(root, threshold):
        4
      /   \
    10     6
-  /  \     \
+  /  \\     \
  5    8    20
 """
 bush = TreeNode(4, TreeNode(10, TreeNode(5), TreeNode(8)), TreeNode(6, None, TreeNode(20)))
@@ -267,7 +267,7 @@ def find_flower(root, flower):
         /    \
        /      \
      Lily     Daisy
-    /    \        \
+    /    \\        \
 Orchid  Lilac    Dahlia
 """
 
@@ -275,6 +275,234 @@ flower_field = TreeNode("Rose",
                         TreeNode("Lily", TreeNode("Orchid"), TreeNode("Lilac")),
                                 TreeNode("Daisy", None, TreeNode("Dahlia")))
 
-print(find_flower(flower_field, "Lilac"))
-print(find_flower(flower_field, "Hibiscus"))
+# print(find_flower(flower_field, "Lilac"))
+# print(find_flower(flower_field, "Hibiscus"))
+
+
+# Session 2
+from collections import deque 
+
+# Tree Node class
+class TreeNode:
+  def __init__(self, value, key=None, left=None, right=None):
+      self.key = key
+      self.val = value
+      self.left = left
+      self.right = right
+
+def build_tree(values):
+  if not values:
+      return None
+
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
+
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
+
+# Problem 1: Monstera Madness
+         
+def count_odd_splits(root):
+    if not root:
+        return 0
+
+    if not root.left and not root.right:
+        return 1 if root.val % 2 == 1 else 0
+
+    return count_odd_splits(root.left) + count_odd_splits(root.right)
+    
+
+"""
+      2
+     / \
+    /   \
+   3     5
+  / \\     \
+ 6   7     12
+"""
+
+# Using build_tree() function included at top of page
+values = [2, 3, 5, 6, 7, None, 12]
+monstera = build_tree(values)
+
+# print(count_odd_splits(monstera))
+# print(count_odd_splits(None))
+
+
+
+# Problem 2: Flower Finding
+class TreeNode():
+     def __init__(self, value, left=None, right=None):
+         self.val = value
+         self.left = left
+         self.right = right
+         
+def find_flower(inventory, name):
+    if not inventory:
+        return False
+    if inventory.val == name:
+        return True
+    if name < inventory.val:
+        return find_flower(inventory.left, name) 
+    else:
+        return find_flower(inventory.right, name)
+
+
+"""
+          Rose
+         /    \
+      Lilac  Tulip
+      /  \\       \
+   Daisy Lily   Violet
+"""
+
+# using build_tree() function at top of page
+values = ["Rose", "Lilac", "Tulip", "Daisy", "Lily", None, "Violet"]
+garden = build_tree(values)
+
+# print(find_flower(garden, "Lilac"))  
+# print(find_flower(garden, "Sunflower")) 
+
+
+
+# Problem 3: Flower Finding II
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def non_bst_find_flower(root, name):
+    if root is None:
+        return False
+    
+    if root.val == name:
+        return True
+
+    return non_bst_find_flower(root.left, name) or non_bst_find_flower(root.right, name)
+
+"""
+         Daisy
+        /    \
+      Lily   Tulip
+     /  \\       \
+  Rose  Violet  Lilac
+"""
+
+# using build_tree() function at top of page
+values = ["Rose", "Lily", "Tulip", "Daisy", "Lilac", None, "Violet"]
+garden = build_tree(values)
+
+# print(non_bst_find_flower(garden, "Lilac"))  
+# print(non_bst_find_flower(garden, "Sunflower")) 
+
+
+
+# Problem 4: Adding a New Plant to the Collection
+from collections import deque 
+
+# Tree Node class
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+# 
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def add_plant(collection, name):
+    if collection is None:
+        return TreeNode(name)
+    
+    if name < collection.val:
+        collection.left = add_plant(collection.left, name)
+    elif name > collection.val:
+        collection.right = add_plant(collection.right, name)
+    else:
+        collection.right = add_plant(collection.right, name)
+
+    return collection
+
+"""
+            Money Tree
+        /              \
+Fiddle Leaf Fig    Snake Plant
+"""
+
+# Using build_tree() function at the top of page
+values = ["Money Tree", "Fiddle Leaf Fig", "Snake Plant"]
+collection = build_tree(values)
+
+# Using print_tree() function at the top of page
+# print_tree(add_plant(collection, "Aloe"))
+
+
+
+
+# Problem 5: Sorting Plants by Rarity
+class TreeNode:
+    def __init__(self, key, value, left=None, right=None):
+        self.key = key      # Plant price
+        self.val = value      # Plant name
+        self.left = left
+        self.right = right
+
+
+def sort_plants(collection):
+    pass
+
+"""
+         (3, "Monstera")
+        /               \
+   (1, "Pothos")     (5, "Witchcraft Orchid")
+        \                 /
+  (2, "Spider Plant")   (4, "Hoya Motoskei")
+"""
+
+# Using build_tree() function at the top of page
+values = [(3, "Monstera"), (1, "Pothos"), (5, "Witchcraft Orchid"), None, (2, "Spider Plant"), (4, "Hoya Motoskei")]
+collection = build_tree(values)
+
+print(sort_plants(collection))
 
